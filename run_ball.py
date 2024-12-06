e76933060c85a500630765ed6f0e9b5722caf257
Nisara PLOYSUTTIPOL <nisara.pl@ku.th>
2024-12-06 12:11:09 +0700

    haha

--- run_ball.py
+++ run_ball.py
@@ -1,5 +1,5 @@
 import turtle
-import ball
+from ball import Ball
 import random
 import seven_segments_proc
 import heapq
@@ -29,18 +29,6 @@ class RunBall:
 
 # create random number of balls, num_balls, located at random positions within the canvas; each ball has a random
 # velocity value in the x and y direction and is painted with a random color
-    def __predict(self, a):
-        if a is None:
-            return
-        for i in range(len(self.ball_color)):
-            dt = a.time_to_hit(self.ball_color[i])
-            heapq.heappush(self.pq, my_event.Event(self.t + dt, a_ball, self.ball_list[i], None))
-
-            # particle-wall collisions
-        dtX = a.time_to_hit_vertical_wall()
-        dtY = a.time_to_hit_horizontal_wall()
-        heapq.heappush(self.pq, my_event.Event(self.t + dtX, a, None, None))
-        heapq.heappush(self.pq, my_event.Event(self.t + dtY, None, a, None))
 
     def __draw_border(self):
         turtle.penup()
@@ -53,114 +41,41 @@ class RunBall:
             turtle.left(90)
             turtle.forward(2 * self.canvas_height)
             turtle.left(90)
-
-    def __redraw(self):
-        turtle.clear()
-        self.my_paddle.clear()
-        self.__draw_border()
-        self.my_paddle.draw()
-        for i in range(len(self.ball_list)):
-            self.ball_list[i].draw()
-        turtle.update()
-        heapq.heappush(self.pq, my_event.Event(self.t + 1.0 / self.HZ, None, None, None))
-
-    def __paddle_predict(self):
-        for i in range(len(self.ball_list)):
-            a_ball = self.ball_list[i]
-            dtP = a_ball.time_to_hit_paddle(self.my_paddle)
-            heapq.heappush(self.pq, my_event.Event(self.t + dtP, a_ball, None, self.my_paddle))
-
         # move_left and move_right handlers update paddle positions
 
-    def move_left(self):
-        if (self.my_paddle.location[0] - self.my_paddle.width / 2 - 40) >= -self.canvas_width:
-            self.my_paddle.set_location([self.my_paddle.location[0] - 40, self.my_paddle.location[1]])
+    def __run(self):
+        for i in range(self.num_ball):
+            self.xpos.append(random.uniform(-1 * self.canvas_width + self.ball_radius,
+                                            self.canvas_width - self.ball_radius))
+            self.ypos.append(random.uniform(-1 * self.canvas_height + self.ball_radius,
+                                            self.canvas_height - self.ball_radius))
+            self.vx.append(10 * random.uniform(-1.0, 1.0))
+            self.vy.append(10 * random.uniform(-1.0, 1.0))
+            self.ball_color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
-        # move_left and move_right handlers update paddle positions
-
-    def move_right(self):
-        if (self.my_paddle.location[0] + self.my_paddle.width / 2 + 40) <= self.canvas_width:
-            self.my_paddle.set_location([self.my_paddle.location[0] + 40, self.my_paddle.location[1]])
+    def draw_border(self):
+        turtle.penup()
+        turtle.goto(-self.canvas_width, -self.canvas_height)
+        turtle.pensize(10)
+        turtle.pendown()
+        turtle.color((0, 0, 0))
+        for i in range(2):
+            turtle.forward(2 * self.canvas_width)
+            turtle.left(90)
+            turtle.forward(2 * self.canvas_height)
+            turtle.left(90)
 
     def run(self):
-        # initialize pq with collision events and redraw event
-        for i in range(len(self.ball_list)):
-            self.__predict(self.ball_list[i])
-        heapq.heappush(self.pq, my_event.Event(0, None, None, None))
-
-        # listen to keyboard events and activate move_left and move_right handlers accordingly
-        self.screen.listen()
-        self.screen.onkey(self.move_left, "Left")
-        self.screen.onkey(self.move_right, "Right")
-
+        dt = 0.2  # time step
         while (True):
-            e = heapq.heappop(self.pq)
-            if not e.is_valid():
-                continue
+            turtle.clear()
+            self.draw_border()
+            ball = Ball
+            for i in range(self.num_ball):
+                ball.draw_ball(self.ball_color[i], self.ball_radius, self.xpos[i], self.ypos[i])
+                ball.move_ball(i, self.xpos, self.ypos, self.vx, self.vy, dt)
+                ball.update_ball_velocity(i, self.xpos, self.ypos, self.vx, self.vy, self.canvas_width,
+                                          self.canvas_height, self.ball_radius)
+            turtle.update()
 
-            ball_a = e.a
-            ball_b = e.b
-            paddle_a = e.paddle
-
-            # update positions, and then simulation clock
-            for i in range(len(self.ball_list)):
-                self.ball_list[i].move(e.time - self.t)
-            self.t = e.time
-
-            if (ball_a is not None) and (ball_b is not None) and (paddle_a is None):
-                ball_a.bounce_off(ball_b)
-            elif (ball_a is not None) and (ball_b is None) and (paddle_a is None):
-                ball_a.bounce_off_vertical_wall()
-            elif (ball_a is None) and (ball_b is not None) and (paddle_a is None):
-                ball_b.bounce_off_horizontal_wall()
-            elif (ball_a is None) and (ball_b is None) and (paddle_a is None):
-                self.__redraw()
-            elif (ball_a is not None) and (ball_b is None) and (paddle_a is not None):
-                ball_a.bounce_off_paddle()
-
-            self.__predict(ball_a)
-            self.__predict(ball_b)
-
-            self.__paddle_predict()
-
-        # hold the window; close it by clicking the window close 'x' mark
-        turtle.done()
-
-
-# num_balls = int(input("Number of balls to simulate: "))
-num_balls = 10
-my_simulator = RunBall(num_balls)
-my_simulator.run()
-"""
-for i in range(num_balls):
-    xpos.append(random.uniform(-1*canvas_width + ball_radius, canvas_width - ball_radius))
-    ypos.append(random.uniform(-1*canvas_height + ball_radius, canvas_height - ball_radius))
-    vx.append(10*random.uniform(-1.0, 1.0))
-    vy.append(10*random.uniform(-1.0, 1.0))
-    ball_color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-
-def draw_border():
-    turtle.penup()
-    turtle.goto(-canvas_width, -canvas_height)
-    turtle.pensize(10)
-    turtle.pendown()
-    turtle.color((0, 0, 0))
-    for i in range(2):
-        turtle.forward(2*canvas_width)
-        turtle.left(90)
-        turtle.forward(2*canvas_height)
-        turtle.left(90)
-
-dt = 0.2 # time step
-while (True):
-    turtle.clear()
-    draw_border()
-    for i in range(num_balls):
-        ball.draw_ball(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_ball(i, xpos, ypos, vx, vy, dt)
-        ball.update_ball_velocity(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
-    turtle.update()
-
-# hold the window; close it by clicking the window close 'x' mark
-turtle.done()
-"""
+    turtle.done()
